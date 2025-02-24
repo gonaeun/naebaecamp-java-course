502b3d4c87e2385872c19d394ecce29518a61414
고나은 <gonaeunn@gmail.com>
2025-02-24 17:57:49 +0900

    java 프로젝트 관리

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import chapter1.MyNewClass; // new MyNewClass();를 입력하면 어느 패키지에 정의된 파일인지 명시됨
+
 public class Main {
     // 모든 것이 class(객체)로 이루어짐 >> Java는 객체지향적!
 
     public static void main(String[] args) {
         // (주석) 코드에 대한 설명 작성
         System.out.println("Hello, Java!");
+
+        new MyNewClass(); // 패키지 활용 방법
     }
 }
--- src/MyNewClass.java
+++
@@ -1,3 +0,0 @@
-public class MyNewClass {
-// Java에서 모든 코드들은 Class 안에 위치함
-}
