3441d7c4e5096261dacbb8ffbfa3904ee2daa041
고나은 <gonaeunn@gmail.com>
2025-02-24 17:31:57 +0900

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    // 모든 것이 class(객체)로 이루어짐 >> Java는 객체지향적!
+
+    public static void main(String[] args) {
+        // (주석) 코드에 대한 설명 작성
+        System.out.println("Hello, Java!");
+    }
+}
---
+++ src/MyNewClass.java
@@ -0,0 +1,3 @@
+public class MyNewClass {
+// Java에서 모든 코드들은 Class 안에 위치함
+}
