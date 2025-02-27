1b40f62a26d4948761ef03c4a27647549d4e45d8
고나은 <gonaeunn@gmail.com>
2025-02-27 18:25:49 +0900

    오답정리) 코드 간결하게!
    board[i][j] 값 자체가 boolean이므로,  == true를 적지 않는게 더 효율적!

--- src/chapter1/array/arrayTest3.java
+++ src/chapter1/array/arrayTest3.java
@@ -10,7 +10,7 @@ public class arrayTest3 {
 
         for (int i=0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == true) {
+                if (board[i][j]) {
                     System.out.println("검은돌(●) 위치 : (" + i + "," + j + ")" );
                 }
             }
