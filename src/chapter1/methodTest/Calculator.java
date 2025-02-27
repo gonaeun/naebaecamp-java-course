2b140b998e156454fd4c3e40fc791185454e8ca4
고나은 <gonaeunn@gmail.com>
2025-02-27 20:29:09 +0900

    계산기 실습

---
+++ src/chapter1/methodTest/Calculator.java
@@ -0,0 +1,11 @@
+package chapter1.methodTest;
+
+public class Calculator {
+
+    // 기능1 : 더하기 메서드
+    int sum(int a, int b) {
+        int result = a + b;
+        System.out.println("더하기 결과 : " + result);
+        return result;
+    }
+}
---
+++ src/chapter1/methodTest/Main.java
@@ -0,0 +1,9 @@
+package chapter1.methodTest;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator(); // calculator 객체화
+
+        calculator.sum(1,2); // 메서드 호출
+    }
+}
