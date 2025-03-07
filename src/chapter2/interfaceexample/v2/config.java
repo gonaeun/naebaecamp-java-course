71045cd5d114281f59eb34478ed50833d85c1c3b
고나은 <gonaeunn@gmail.com>
2025-03-07 17:35:52 +0900

    Update config.java

--- src/chapter2/interfaceexample/v2/config.java
+++ src/chapter2/interfaceexample/v2/config.java
@@ -1,7 +1,7 @@
 package chapter2.interfaceexample.v2;
 
 public interface config {
-    // 인터페이스의 변수는 public static final로 선언됨\
+    // 인터페이스의 변수는 public static final로 선언됨
     // 하지만 변수 선언을 권장하진 않음. 인터페이스는 표준을 제공하는 역할이기 때문
     int POPULATION = 100;
 }
