993fa396632cc943aea7a870549b5be1f6c18dcd
고나은 <gonaeunn@gmail.com>
2025-03-10 16:56:16 +0900

    객체지향 - 캡슐화

---
+++ src/chapter2/capsulation/Main.java
@@ -0,0 +1,10 @@
+package chapter2.capsulation;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 생성자 호출
+        Person person = new Person();
+
+    }
+}
---
+++ src/chapter2/capsulation/Person.java
@@ -0,0 +1,16 @@
+package chapter2.capsulation;
+
+public class Person {
+
+    // 속성
+    public String name;
+    private String secret;
+
+    // 생성자
+    public Person() {}
+    // 접근제어자를 명시하지 않으면 >> 기본 접근제어자는 default
+
+    // 기능
+    public void methodA() {}
+    public void methodB() {}
+}
