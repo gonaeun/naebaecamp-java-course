aa109c2eab3fba432689df571e81ba4f5072ed04
고나은 <gonaeunn@gmail.com>
2025-03-10 19:19:52 +0900

    캡슐화의 정보은닉과 접근제어자 활용

--- src/chapter2/capsulation/Main.java
+++ src/chapter2/capsulation/Main.java
@@ -6,5 +6,21 @@ public class Main {
         // 생성자 호출
         Person person = new Person();
 
+        // 인스턴스 변수 접근
+        person.name = "gonaeun";
+//        person.secret = "??" //private 이라서 접근 안됨
+
+        // 인스턴스 메서드 접근
+        person.methodA();
+//        person.methodB(); //private 이라서 접근 안됨
+
+        // 게터
+        String name = person.getName();
+        System.out.println("이름 : "  + name);
+
+        // 세터
+        person.setName("hanni"); // 인스턴스 변수에 대한 값을 변경하고 싶을 때 세터 사용
+        String name2 = person.getName();
+        System.out.println("이름 : " + name2);
     }
 }
--- src/chapter2/capsulation/Person.java
+++ src/chapter2/capsulation/Person.java
@@ -8,9 +8,28 @@ public class Person {
 
     // 생성자
     public Person() {}
-    // 접근제어자를 명시하지 않으면 >> 기본 접근제어자는 default
+//    private Person() {}
+    // private 접근제어자(접근제어범위 : 같은 클래스) >> main에서 호출 오류
+//    Person() {}
+    // 접근제어자를 명시하지 않으면 >> 기본 접근제어자는 default(접근제어범위 : 패킺지 내부)
+
+    // 속성들을 private으로 설정해두고 생성자를 public으로 열어두면 매개변수를 통해 생성시점에 한번에 설정하므로 더욱 효율적
+    public Person(String name, String secret) {
+        this.name = name;
+        this.secret = secret;
+    }
 
     // 기능
     public void methodA() {}
-    public void methodB() {}
+    private void methodB() {}
+
+    // 게터
+    public String getName() {
+        return name;
+    }
+
+    // 세터
+    public void setName(String name) {
+        this.name = name;
+    }
 }
