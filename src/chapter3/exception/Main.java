471845b50801ce153931b64fd681fc2e5ccdd227
고나은 <gonaeunn@gmail.com>
2025-03-12 19:03:13 +0900

    로그인 예외처리해보기

--- src/chapter3/exception/Main.java
+++ src/chapter3/exception/Main.java
@@ -1,5 +1,7 @@
 package chapter3.exception;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -34,5 +36,29 @@ public class Main {
 
 
         System.out.println("프로그램 종료"); // 이전코드에서 예외처리되야 실행될 수 있음
+
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print("아이디를 입력하세요");
+                String userid = scanner.next();
+                System.out.print("비밀번호를 입력하세요");
+                String password = scanner.next();
+
+                login(userid, password); // 예외 발생시 catch에서 처리해야해
+                System.out.println("로그인 성공!");
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());   // Exception애 전달된 문자열이 e.getMessage()에 저장됨
+            }
+        }
+    }
+
+    public static void login (String userid, String password) throws Exception {
+        if (!userid.equals("admin") || !password.equals("1234")) {
+            throw new Exception("로그인 실패! 아이디 또는 비밀번호가 잘못되었습니다.");
+        }
     }
 }
+
